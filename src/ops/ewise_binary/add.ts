de8aae185e0084d2b86521efdac9f18bb4393e71
hongjiaherng <jiaherng2001@gmail.com>
2023-02-14 19:38:28 +0800

    fix add method

--- src/ops/ewise_binary/add.ts
+++ src/ops/ewise_binary/add.ts
@@ -1,5 +1,7 @@
 import { Tensor } from "../../tensor";
 import { DType, TensorLike, RecursiveArray } from "../../types";
+import { upcastType } from "../../types_util";
+import { broadcastTensors, TensorsIterator } from "../broadcast";
 import { tensor } from "../creation";
 
 export function add<D extends DType>(
@@ -11,30 +13,25 @@ export function add<D extends DType>(
   // determine target dtype
   // create new tensor with target dtype
 
-  // let input_ = input instanceof Tensor ? input : tensor(input);
-  // let other_ = other instanceof Tensor ? other : tensor(other);
+  let input_ = input instanceof Tensor ? input : tensor(input);
+  let other_ = other instanceof Tensor ? other : tensor(other);
 
-  // [input_, other_] = broadcastTensors(input_, other_);
+  [input_, other_] = broadcastTensors(input_, other_);
 
-  // const targetArray = [];
-  // const targetDType = upcastType(input_.dtype, other_.dtype);
+  const targetArray = [];
+  const targetDType = upcastType(input_.dtype, other_.dtype);
 
-  // const tensorsIterator = new TensorsIterator(input_, other_);
+  const tensorsIterator = new TensorsIterator(input_, other_);
 
-  // if (targetDType === "bool") {
-  //   for (const [inputVal, otherVal] of tensorsIterator) {
-  //     targetArray.push((inputVal as boolean) || (otherVal as boolean));
-  //   }
-  // } else {
-  //   for (const [inputVal, otherVal] of tensorsIterator) {
-  //     targetArray.push(Number(inputVal) + Number(otherVal));
-  //   }
-  // }
+  if (targetDType === "bool") {
+    for (const [inputVal, otherVal] of tensorsIterator) {
+      targetArray.push((inputVal as boolean) || (otherVal as boolean));
+    }
+  } else {
+    for (const [inputVal, otherVal] of tensorsIterator) {
+      targetArray.push(Number(inputVal) + Number(otherVal));
+    }
+  }
 
-  // return fromTensorLike(
-  //   targetArray as TensorLike,
-  //   input_.shape,
-  //   targetDType
-  // ) as Tensor<D>;
-  return input instanceof Tensor ? input : tensor(input);
+  return tensor(targetArray, input_.shape, targetDType) as Tensor<D>;
 }
